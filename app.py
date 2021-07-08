79b5ad49ae163e47855b915b67163e3324b223d6
inka.179 <ewelinadalek@gmail.com>
2021-07-08 16:21:32 +0200

    user resource added

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_jwt import JWT
+
 from security import authenticate, identity
+from resources.user import UserRegister
+#import resources
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = "zmienic to. nie hardcoded ma być"
 
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    
 jwt = JWT(app, authenticate, identity)
+
+
 
 api.add_resource(Locations, '/locations')
 api.add_resource(Location, '/location/<string:name>')
@@ -17,4 +30,7 @@ api.add_resource(Book, '/book/<string:title>')
 api.add_resource(UserRegister, '/register')
 #nie jestem pewna co do endpointów
 
-app.run()
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000)
