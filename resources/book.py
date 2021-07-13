9dac071dbce0c15147e319c13869b8fd537e9dd7
inka.179 <ewelinadalek@gmail.com>
2021-07-13 16:44:41 +0200

    new methods in book resource and added booklist resource

--- resources/book.py
+++ resources/book.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.book import BookModel
+from models.shelf import ShelfModel
 
 class Book(Resource):
     parser = reqparse.RequestParser()
@@ -8,6 +9,41 @@ class Book(Resource):
     parser.add_argument('is_on_shelf', type=bool, required=True, help="This cannot be blank")
     parser.add_argument('publishing_house', type=str, required=True, hel="This field cannot be blank")
     parser.add_argument('publication_date', type=str, required=False)
+
+    def get(self, title):
+        book = BookModel.find_by_title(title)
+        if book:
+            return book.json()
+        return {'message': 'Book not found'}, 404
+
+    def post(self, title):
+        if BookModel.find_by_title(title):
+            return {'message': 'Book {} already exist'.format(title)}, 400
+
+        data = Book.parser.args()
+        book = BookModel.find_by_title(title, **data)
+        try:
+            book.save_to_db()
+        except:
+            return {'message': 'An error occurred creating a book'}, 500
+        return book.json(), 201
+
+    def delete(self, title):
+        book = BookModel.find_by_title(title)
+        if book:
+            book.delete_from_db()
+        return {'message': 'Book deleted'}
+
+    class BookList(Resource):
+        def get(self, shelf_id):
+            books=BookModel.query.filter_by(shelf_id=shelf_id).all()
+            return {'book from shell {}.format(shell_id)': [x.json() for x in books]}
+        #ten cały do poprawki. poczytaj o endpointach
+
+
+
+
+
 
 
 
