11e7d1bada9af1780b6610d294726a5a46b295bf
Ewelina179 <ewelinadalek@gmail.com>
2022-01-04 14:44:21 +0100

    changes in parser

--- resources/book.py
+++ resources/book.py
@@ -7,7 +7,7 @@ class Book(Resource):
     parser.add_argument('author', type=str, required=True, help="This field cannot be blank.")
     parser.add_argument('shelf_id', type=int, required=True, help="This field cannot be blank")
     parser.add_argument('is_on_shelf', type=bool, required=True, help="This cannot be blank")
-    parser.add_argument('publishing_house', type=str, required=True, hel="This field cannot be blank")
+    parser.add_argument('publishing_house', type=str, required=True, help="This field cannot be blank")
     parser.add_argument('publication_date', type=str, required=False)
 
     def get(self, title):
