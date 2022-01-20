cd1a3e8fdf4f0c873aabe20873e5d670056d7d1e
Ewelina179 <ewelinadalek@gmail.com>
2022-01-20 14:41:59 +0100

    changed find_by_title, fixed saving book obj

--- models/book.py
+++ models/book.py
@@ -1,4 +1,13 @@
+from datetime import date
+import json
 from db import db
+
+class ComplexEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, date):
+            return obj.strftime('%Y-%m-%d')
+        else:
+            return json.JSONEncoder.default(self, obj)
 
 class BookModel(db.Model):
     __tablename__ = 'books'
@@ -9,23 +18,25 @@ class BookModel(db.Model):
     shelf_id = db.Column(db.Integer, db.ForeignKey('shelfs.id'))
     is_on_shelf = db.Column(db.Boolean)
     publishing_house = db.Column(db.String(128))
-    publication_date = db.Column(db.DateTime)
+    publication_date = db.Column(db.Date)
     shelf = db.relationship('ShelfModel')
 
     def __init__(self, author, title, shelf_id, is_on_shelf, publishing_house, publication_date):
         self.author = author
         self.title = title
         self.shelf_id = shelf_id
-        self.is_on_shell = is_on_shelf
+        self.is_on_shelf = is_on_shelf
         self.publishing_house = publishing_house
         self.publication_date = publication_date
 
     def json(self):
-        return {'title': self.title, 'author': self.author, 'shelf': self.shelf_id, 'is available': self.is_on_shelf, 'publishing house': self.publishing_house, 'publication date': self.publishing_date}
+        print({'title': self.title, 'author': self.author, 'shelf': self.shelf_id, 'is available': self.is_on_shelf, 'publishing house': self.publishing_house, 'publication date': self.publication_date})
+        return {'title': self.title, 'author': self.author, 'shelf': self.shelf_id, 'is available': self.is_on_shelf, 'publishing house': self.publishing_house, 'publication date': json.dumps(self.publication_date, cls=ComplexEncoder)}
 
     @classmethod
     def find_by_title(cls, title):
-        cls.find_by_title(title=title).all()
+        print(title)
+        return cls.query.filter_by(title=title).first()
 
     def save_to_db(self):
         db.session.add(self)
--- resources/book.py
+++ resources/book.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 from models.book import BookModel
 from models.shelf import ShelfModel
+from datetime import datetime
 
 class Book(Resource):
     parser = reqparse.RequestParser()
@@ -11,6 +12,7 @@ class Book(Resource):
     parser.add_argument('publication_date', type=str, required=False)
 
     def get(self, title):
+        print(title)
         book = BookModel.find_by_title(title)
         if book:
             return book.json()
@@ -19,11 +21,35 @@ class Book(Resource):
     def post(self, title):
         if BookModel.find_by_title(title):
             return {'message': 'Book {} already exist'.format(title)}, 400
+        print("hej")
+        data = Book.parser.parse_args()
+        author = data["author"]
+        shelf_id = data["shelf_id"]
+        is_on_shelf = data['is_on_shelf']
+        publishing_house = data['publishing_house']
+        publication_date = data['publication_date']
+        print("hejj")
+        print(shelf_id)
+        shelf = ShelfModel.query.filter_by(id=shelf_id).first()
+        print(f"to {shelf.id} id tej półki")
+        
+        expiration_year  = int(publication_date[:4])
+        expiration_month =  int(publication_date[5:7])
+        expiration_date = int(publication_date[8:10])
+        expiration_date =datetime(expiration_year,expiration_month,expiration_date)
 
-        data = Book.parser.args()
-        book = BookModel.find_by_title(title, **data)
+        # najpierw sprawdź czy półka jest w bazie. jeśli nie, error, jeśli tak, wstaw jej id do obiektu book i to dalej
+        book = BookModel(author=author, title=title, shelf_id=shelf.id, is_on_shelf=is_on_shelf, publishing_house=publishing_house, publication_date=expiration_date)
+        print(book.author)
+        print(book.title)
+        print(book.shelf_id)
+        print(book.is_on_shelf)
+        print(book.publishing_house)
+        print(book.publication_date)
+
         try:
             book.save_to_db()
+            print("hejo")
         except:
             return {'message': 'An error occurred creating a book'}, 500
         return book.json(), 201
@@ -38,7 +64,6 @@ class Book(Resource):
         def get(self, shelf_id):
             books=BookModel.query.filter_by(shelf_id=shelf_id).all()
             return {'book from shell {}.format(shell_id)': [x.json() for x in books]}
-        #ten cały do poprawki. poczytaj o endpointach
 
 
 
