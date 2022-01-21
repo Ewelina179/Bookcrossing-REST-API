0dfecbe155f6c2b07f19d38e80b32a9e5e711fe3
Ewelina179 <ewelinadalek@gmail.com>
2022-01-21 10:51:03 +0100

    changed post and delete method

--- resources/shelf.py
+++ resources/shelf.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.shelf import ShelfModel
+from models.city import CityModel
 
 class Shelf(Resource):
     parser = reqparse.RequestParser()
@@ -17,6 +18,12 @@ class Shelf(Resource):
             return {'message': 'A shelf {} already exists.'.format(name)}, 400
         
         data = Shelf.parser.parse_args()
+        
+        city_id = data["city_id"]
+        if not CityModel.find_by_id(city_id):
+            return {'message': 'A city with {} id not exist in db.'.format(city_id)}, 400
+
+
         shelf = ShelfModel(name, **data)
         try:
             shelf.save_to_db()
@@ -28,5 +35,6 @@ class Shelf(Resource):
         shelf = ShelfModel.find_by_name(name)
         if shelf:
             shelf.delete_from_db()
-
-        return {'message': 'Shelf deleted'}
+            return {'message': 'Shelf deleted'}
+        else:
+            return {'message': 'Shelf not in db'}
