93c4966f25b82527313437ff11c326cbdcfe05ba
Ewelina179 <ewelinadalek@gmail.com>
2022-01-21 13:56:51 +0100

    changed database_uri

--- app.py
+++ app.py
@@ -14,7 +14,10 @@ from resources.book import Book, BookList, BookShelfList
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+database_path = os.environ.get('DATABASE_URL', 'sqlite:///data.db') 
+if database_path.startswith("postgres://"):
+    database_path = database_path.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = "s key"
